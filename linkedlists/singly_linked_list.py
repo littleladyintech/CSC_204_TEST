a692aa0a08ddd58cd118d039e18324b8ce8d6acd
Alliu Maryam Abiola <119784113+littleladyintech@users.noreply.github.com>
2023-07-26 08:05:23 +0100

    CSC 204
    UPDATED CSC 204

--- linkedlists/singly_linked_list.py
+++ linkedlists/singly_linked_list.py
@@ -5,15 +5,11 @@ from binarytree import build
 #TASK 1
 class SinglyLinkedList:
     def __init__(self):
-        """
-        Initialize an empty singly linked list.
-        """
+        #Initialize an empty singly linked list.
         self.head = None
 
     def insert(self, data):
-        """
-        Insert a new element at the end of the linked list.
-        """
+    #Insert a new element at the end of the linked list.
         new_node = Node(data)
         if self.head is None:
             self.head = new_node
@@ -24,26 +20,13 @@ class SinglyLinkedList:
             current.next = new_node
 
     def display(self):
-        """
-        Display the elements of the linked list.
-        """
+        #Display the elements of the linked list.
         elements = []
         current = self.head
         while current:
             elements.append(current.data)
             current = current.next
         print(elements)
- #JUST TO CHECK FOR THE LENGTH OF THE LINKED LIST
-    def length(self):
-        """
-        Calculate the length of the linked list.
-        """
-        count = 0
-        current = self.head
-        while current:
-            count += 1
-            current = current.next
-        return count
 
     # TASK 2
     def find_max_min(self):
@@ -90,3 +73,59 @@ class SinglyLinkedList:
         root = build(elements)
 
         return root
+    #Implementing a binary search algorithm
+    def binary_search(self, search_value):
+        # Sort the linked list using Python's built-in sorting function
+        sorted_list = []
+        current = self.head
+        while current:
+            sorted_list.append(current.data)
+            current = current.next
+        sorted_list.sort()
+
+        # Binary search on the sorted list
+        low, high = 0, len(sorted_list) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if sorted_list[mid] == search_value:
+                return mid
+            elif sorted_list[mid] < search_value:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return -1
+#Implementing Queue using a Singly Linked List
+
+class Queue:
+    def __init__(self):
+        self.sll = SinglyLinkedList()
+
+    def enqueue(self, data):
+        self.sll.insert(data)
+
+    def dequeue(self):
+        if self.is_empty():
+            return None
+        data = self.sll.head.data
+        self.sll.head = self.sll.head.next
+        return data
+
+    def display(self):
+        self.sll.display()
+
+    def is_empty(self):
+        return self.sll.head is None
+#Sort the Queue
+    def sort_queue(self):
+        # Extract elements from the queue
+        elements = []
+        while not self.is_empty():
+            elements.append(self.dequeue())
+
+        # Sort the elements
+        elements.sort()
+
+        # Re-enqueue the sorted elements
+        for element in elements:
+            self.enqueue(element)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # main.py
 from linkedlists.singly_linked_list import SinglyLinkedList
+from linkedlists.singly_linked_list import Queue
 
 def main():
     data_list = [1, 3, 4, 5, 6, 8, 7, 9, 2, 22, 15, 55, 45, 23, 24, 26, 28]
@@ -13,9 +14,8 @@ def main():
 
     # Display the elements of the linked list
     linked_list.display()
+ 
 
-    # Test other list operations
-    print("Length of the linked list:", linked_list.length())
 
 #TASK 2
 def main():
@@ -61,3 +61,25 @@ def main():
 
 if __name__ == "__main__":
     main()
+    #TASK 3(ii)
+# Testing Queueing operations
+def main():
+    data_list = [1, 3, 4, 5, 6, 8, 7, 9, 2, 22, 15, 55, 45, 23, 24, 26, 28]
+
+    queue = Queue()
+    for data in data_list:
+        queue.enqueue(data)
+
+    print("\nQueue:")
+    queue.display()
+
+    print("Dequeue:", queue.dequeue())
+
+    print("Queue after dequeue:")
+    queue.display()
+
+    queue.sort_queue()
+    print("\nSorted Queue:")
+    queue.display()
+    if __name__ == "__main__":
+     main()
